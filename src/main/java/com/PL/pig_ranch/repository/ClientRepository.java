ee2ba10316a383ff30f80951faf798c0b5287e1e
parker <pliabraaten@gmail.com>
2026-02-24 08:44:09 -0800

    Remove unused findHouseholdIdByClientId from ClientRepository

--- src/main/java/com/PL/pig_ranch/repository/ClientRepository.java
+++ src/main/java/com/PL/pig_ranch/repository/ClientRepository.java
@@ -2,8 +2,6 @@ package com.PL.pig_ranch.repository;
 
 import com.PL.pig_ranch.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,6 +10,4 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
 
     long countByHouseholdId(Long householdId);
 
-    @Query(value = "SELECT household_id FROM client WHERE id = :clientId", nativeQuery = true)
-    Long findHouseholdIdByClientId(@Param("clientId") Long clientId);
 }
