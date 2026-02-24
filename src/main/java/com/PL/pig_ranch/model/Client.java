b3abc1f642e9fd2012f5dbdc63916cd3eb0250d5
parker <pliabraaten@gmail.com>
2026-02-24 08:42:11 -0800

    refactor: Replace @Data with explicit @Getter/@Setter and @EqualsAndHashCode on entities to prevent JPA recursion issues

--- src/main/java/com/PL/pig_ranch/model/Client.java
+++ src/main/java/com/PL/pig_ranch/model/Client.java
@@ -3,17 +3,22 @@ package com.PL.pig_ranch.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @NotBlank(message = "Name is required")
--- src/main/java/com/PL/pig_ranch/model/Household.java
+++ src/main/java/com/PL/pig_ranch/model/Household.java
@@ -2,21 +2,26 @@ package com.PL.pig_ranch.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Household {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String surname;
@@ -26,7 +31,8 @@ public class Household {
     private String zipCode;
 
     @ToString.Exclude
-    @OneToMany(mappedBy = "household", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "household", cascade = { CascadeType.PERSIST, CascadeType.MERGE }) // Cascade saves but
+                                                                                             // doesn't delete clients
     private List<Client> members = new ArrayList<>();
 
     @ToString.Exclude
