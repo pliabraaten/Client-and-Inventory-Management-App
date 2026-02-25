9875b0e6cf992a0dcd912cd8947dd87c77900bc1
parker <pliabraaten@gmail.com>
2026-02-25 07:39:20 -0800

    General code cleanup

--- src/main/java/com/PL/pig_ranch/service/HogService.java
+++ src/main/java/com/PL/pig_ranch/service/HogService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface HogService {
+
     List<HogInventory> getAllHogs();
+
     Optional<HogInventory> getHogById(Long id);
+
     HogInventory saveHog(HogInventory hog);
+
     void deleteHog(Long id);
 }
--- src/main/java/com/PL/pig_ranch/service/InventoryService.java
+++ src/main/java/com/PL/pig_ranch/service/InventoryService.java
@@ -6,11 +6,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface InventoryService {
+
     List<InventoryItem> getAllItems();
+
     Optional<InventoryItem> getItemById(Long id);
+
     InventoryItem saveItem(InventoryItem item);
+
     void deleteItem(Long id);
-    
+
     InventoryItem updateStock(Long itemId, int amount, InventoryTransaction.TransactionType type, String notes);
+
     List<InventoryTransaction> getTransactionHistory(Long itemId);
 }
